ee4b4d0099004acfae54299526e4082a77f6b57a
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-05-03 11:48:34 -0400

    Implemented basic parsing

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bank-ocr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Implementation of the Bank OCR Kata"
+  :url "http://codingdojo.org/cgi-bin/index.pl?KataBankOCR"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main bank-ocr.core)
--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -1,6 +1,30 @@
 (ns bank-ocr.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def base-digits
+  ["    _  _     _  _  _  _  _ "
+   "  | _| _||_||_ |_   ||_||_|"
+   "  ||_  _|  | _||_|  ||_| _|"])
+
+;; Split the 4 lines above in strings of 3 chars each
+(defn in-triplets [lines]
+  (map #(re-seq #".{3}" %) lines))
+
+;; Group the collections above (first items from each line, 2nd from each line...)
+(defn digit-keys [triplets]
+  (apply map str triplets))
+
+;; Print a digit from above with:
+;; (println (interpose "\n" (re-seq #".{3}" (nth digit-keys 4))))
+
+(def string->digit
+  (zipmap (digit-keys (in-triplets base-digits)) ;; keys
+          (map str (range 1 (inc 9)))))          ;; values
+
+(def reversed-nums
+  [" _  _  _  _  _     _  _       "
+   "|_||_|  ||_ |_ |_| _| _|  ||_|"
+   " _||_|  ||_| _|  | _||_   |  |"])
+
+(defn -main [& args]
+  (let [parsed-keys (digit-keys (in-triplets reversed-nums))]
+    (println (apply str (map string->digit parsed-keys)))))
